c8e49153a09081b323dc51e560fa5767b0575ae2
chamiyeah <gachampika@live.com>
2023-09-25 23:39:19 +0200

    Configured & connected the Azure Function. Updates to the folder structure

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount ();
 })
 
-const functionApi ='http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azureportfoliocounter.azurewebsites.net/api/GetResumeCounter?code=mWhZH0AuGdN4Rf0MkvMqbgPAr5kW4FPUjImMopJX7qJFAzFuQBBUpQ=='
+const localfunctionApi ='http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 1;
-    fetch(functionApi).then(response =>{
+    fetch(functionApiUrl).then(response =>{
       return response.json ()  
     }).then (response => {
         console.log("Website Called functionApi");
